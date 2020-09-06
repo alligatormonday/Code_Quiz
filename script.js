e1ccebe2b914629300fe5124c5c83cd6d6d8ddf4
alligatormonday <joey@joeyjepson.com>
2020-09-05 21:06:02 -0700

    added a game over screen and made 'begin' quiz button/text hidden once quiz starts.

--- script.js
+++ script.js
@@ -13,6 +13,7 @@
 // WHEN the game is over
 // THEN I can save my initials and score
 // ```
+const quiz = document.getElementById("quiz");
 
 const quizData = [
     {
@@ -59,19 +60,27 @@ const quizData = [
 
 const renderQuizBtn = document.querySelector("#begin_quiz");
 
+const buttonA = document.querySelector("#a");
+
+const buttonB = document.querySelector("#b");
+
+const buttonC = document.querySelector("#c");
+
+const buttonD = document.querySelector("#d");
+
+const title = document.getElementById("quiz_title");
+
+const answers = document.querySelectorAll(".answer");
+
+let time = 75;
+
 let current_question = 0;
 
-const buttonA = document.querySelector("#a")
+let renderQuizEl = document.querySelector("#quiz_questions");
 
-const buttonB = document.querySelector("#b")
-
-const buttonC = document.querySelector("#c")
-
-const buttonD = document.querySelector("#d")
-
-let time = 60;
-
-let renderQuizEl = document.querySelector("#quiz_questions")
+function hideTitle() {
+    document.getElementById("quiz_title").style.display = "none";
+}
 
 function renderQuizQuestion() {
     renderQuizEl.innerHTML = `
@@ -87,13 +96,17 @@ function renderQuizQuestion() {
 // User clicks begin to kick off the quiz
 // Attach on click to index.html for begin button
 renderQuizBtn.addEventListener("click", function () {
+    hideTitle();
     // Start the timer
     let quizTimer = setInterval(
         function () {
             time--;
             document.querySelector("#timer").innerHTML = time;
             if (time === 0) {
-                clearInterval(quizTimer)
+                clearInterval(quizTimer);
+                quiz.innerHTML = `
+                <h3>time is up</h3>
+                `
             }
         }, 1000);
 
@@ -102,23 +115,46 @@ renderQuizBtn.addEventListener("click", function () {
 
 })
 
-const answers = document.querySelectorAll(".answer");
+// function getSelected() {
+//     let answer = undefined;
+
+//     answers.forEach((answerEl) => {
+//         if (answerEl.checked) {
+//             answer = answerEl.id;
+//         }
+//     });
+
+//     return answer;
+// }
+
+// function answerSelected() {
+//     let userSelection = getSelected();
+//     if (userSelection !== quizData[current_question].correct) {
+//         document.querySelector("#timer").innerHTML = time;
+//         time = time - 10;
+//         time.textContent = time;
+//     }
+    
+// }
+
+// answerSelected();
 
 for (let i = 0; i < answers.length; i++) {
     answers[i].addEventListener("click", function () {
         current_question++;
         if (current_question >= quizData.length) {
-            console.log(current_question)
             // Game is over
+            quiz.innerHTML = `
+            <h3>Game over</h3>
+            `
         }
         else {
             renderQuizQuestion();
         }
+    
     })
 
 }
-
-
 
 // Timer starts to countdown
 // Generate a timer that counts down from 60 seconds
