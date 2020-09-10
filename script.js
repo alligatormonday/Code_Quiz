193722910e0cb0d3f1d90ba7b76487d88710df01
alligatormonday <joey@joeyjepson.com>
2020-09-09 19:52:33 -0700

    Finalized README.md for final pass and homework submission.

--- script.js
+++ script.js
@@ -101,7 +101,12 @@ renderQuizBtn.addEventListener("click", function () {
   });
 
   hideTitle();
+
   // Start the timer
+  // Timer starts to countdown
+  // Generate a timer that counts down from 60 seconds
+  // set interval, clear interval,
+  // assign set interval to a variable, allows us to execute clear interval by passing in the variable name
   quizTimer = setInterval(function () {
     time--;
     document.querySelector("#timer").innerHTML = time;
@@ -137,11 +142,19 @@ renderQuizBtn.addEventListener("click", function () {
   renderQuizQuestion();
 });
 
+// Question 1 is rendered
+// Populate question 1 with buttons for answer selection
+// Indicate to the user that the question is correct or incorrect
+// Then populate the next question until quiz is completed
+// If user does not select an answer and presses submit, alert user to answer the question
+// If user answers the question incorrectly, then 50 seconds is deducted from the timer
 for (let i = 0; i < answers.length; i++) {
   answers[i].addEventListener("click", function () {
     if (answers[i].id !== quizData[current_question].correct) {
       time -= 50;
     }
+// Once answer is submitted, the next question is rendered.
+// Users answer selection is saved and updates the quiz data to move from q1 to q2, etc.
     current_question++;
     if (current_question >= quizData.length) {
       // Game is over
@@ -150,6 +163,11 @@ for (let i = 0; i < answers.length; i++) {
             `;
       clearInterval(quizTimer);
       //   console.log(clearInterval(quizTimer));
+
+// Once all questions are answered or timer reaches 0 then the game is over
+// User can save their initials and score
+// Score is based on time left on the clock
+// Once user submits their initials, they can view their score on the 'High Scores.html' page. 
       highscore.innerHTML = `
     <form method="highscore_save"> 
     <label for ="initials">Type your initials and click 'submit' to save your score!</label>
@@ -187,41 +205,3 @@ for (let i = 0; i < answers.length; i++) {
   });
 }
 // Click Events end
-
-// Timer starts to countdown
-// Generate a timer that counts down from 60 seconds
-// set interval, clear interval, set timeout
-// - set interval - a function that executes after a specified interval of milliseconds
-// assign set interval to a variable, allows us to execute clear interval by passing in the variable name
-// - set timeout - if ever you want to execute code after a delay
-
-// Question 1 is rendered
-// Populate question 1 with buttons for answer selection
-// Indicate to the user that the question is correct or incorrect
-// Then populate the next question until quiz is completed
-// If user does not select an answer and presses submit, alert user to answer the question
-// If user answers the question incorrectly, then 15 seconds is deducted from the timer
-
-// Once answer is submitted, the next question is rendered.
-// Users answer selection is saved and updates the quiz data to move from q1 to q2, etc.
-
-// Once all questions are answered or timer reaches 0 then the game is over
-// User can save their initials and score
-// Score is based on time left on the clock
-// Once user submits their initials, they can see the cumulative scores ranked by highest to lowest
-
-// ## Acceptance Criteria
-
-// ```
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and score
-// ```
